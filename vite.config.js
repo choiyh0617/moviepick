ba67557d46ddcd0c0c4094da907c0fb4b396d36c
choiyh0617 <6l0mrc00057@gmail.com>
2025-05-27 09:53:57 +0900

    run

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,5 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: {
-      '/api/v1': {
-        target: 'https://dontouchme.p-e.kr',
-        changeOrigin: true,
-        rewrite: (path) => path,
-        secure: false,
-        ws: true,
-      },
-    },
   },
 });
